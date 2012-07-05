f6774d1067a84e460b66a61e55a48e05913df3ba
Max Zernov <maksdesigncorp@gmail.com>
2012-07-05 09:12:12 +0400

    Added taktika

---
+++ taktika/js/carouselStart.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	$('#icarousel').iCarousel({
+		slides:3 ,
+		slidesSpace: 200,
+		nextLabel: "Nachste",
+		previousLabel: "Vorherige",
+		playLabel: "Spielen",
+		pauseLabel: "Pause",
+		timer: "360Bar",
+		timerPadding: 3,
+		timerOpacity: 0.5,
+		timerColor: "#0F0",
+		timerX: 15,
+		timerY: 30
+	});
+});
